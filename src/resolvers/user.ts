1d7a95061d9c4635289f490b047647069e2bf161
Duong Minh Quy <31639013+duongminhquyltv@users.noreply.github.com>
2022-08-15 17:47:58 +0700

    Add module register user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,21 +1,39 @@
 import { User } from "../entities/User";
 import { Resolver, Mutation, Arg } from "type-graphql";
 import argon2 from "argon2";
+import { UserMutationResponse } from "../types/UserMutationResponse";
+import { RegisterInput } from "../types/RegisterInput";
+import { validateRegisterInput } from "../utils/validateRegisterInput";
 
 @Resolver()
 export class UserResolver {
-  @Mutation((_returns) => User, { nullable: true })
+  @Mutation((_returns) => UserMutationResponse)
   async register(
-    @Arg("email") email: string,
-    @Arg("username") username: string,
-    @Arg("password") password: string
-  ): Promise<User | null> {
+    @Arg("registerInput") registerInput: RegisterInput
+  ): Promise<UserMutationResponse> {
+    const validateRegisterInputErrors = validateRegisterInput(registerInput);
+    if (validateRegisterInputErrors !== null)
+      return { code: 400, success: false, ...validateRegisterInputErrors };
+
     try {
+      const { username, email, password } = registerInput;
       const userExist = await User.findOne({
         where: [{ username }, { email }],
       });
       if (userExist) {
-        return null;
+        return {
+          code: 400,
+          success: false,
+          message: "Duplicated username or email",
+          errors: [
+            {
+              field: userExist.username === username ? "username" : "email",
+              message: `${
+                userExist.username === username ? "Username" : "Email"
+              } already taken.`,
+            },
+          ],
+        };
       }
 
       const hashedPassword = await argon2.hash(password);
@@ -26,10 +44,18 @@ export class UserResolver {
         password: hashedPassword,
       });
 
-      return await User.save(newUser);
+      return {
+        code: 200,
+        success: true,
+        message: "User registration successful",
+        user: await User.save(newUser),
+      };
     } catch (error) {
-      console.log(error);
-      return null;
+      return {
+        code: 500,
+        success: false,
+        message: `Inernal server error ${error.message}`,
+      };
     }
   }
 }
